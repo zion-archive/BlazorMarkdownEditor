33d61c70ef56d880b57eab6d0aede0eeb641d8fb
wortelus <daniel.slavik.cz@gmail.com>
2025-02-14 03:36:53 +0100

    Relative paths

--- PSC.Blazor.Components.MarkdownEditor/FilePath.cs
+++ PSC.Blazor.Components.MarkdownEditor/FilePath.cs
@@ -8,10 +8,10 @@ namespace PSC.Blazor.Components.MarkdownEditor
 {
     internal static class FilePath
     {
-        internal static readonly string MarkdownJs = "/_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/markdownEditor.js";
-        internal static readonly string EasyMDEJs = "/_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/easymde.min.js";
-        internal static readonly string EasyMDECss = "/_content/PSC.Blazor.Components.MarkdownEditor.Fork/css/easymde.min.css";
-        internal static readonly string MarkedJs = "/_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/marked.min.js";
-        internal static readonly string CodeMirrorThemesUrl = "/_content/PSC.Blazor.Components.MarkdownEditor.Fork/css/codemirror/";
+        internal static readonly string MarkdownJs = "_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/markdownEditor.js";
+        internal static readonly string EasyMDEJs = "_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/easymde.min.js";
+        internal static readonly string EasyMDECss = "_content/PSC.Blazor.Components.MarkdownEditor.Fork/css/easymde.min.css";
+        internal static readonly string MarkedJs = "_content/PSC.Blazor.Components.MarkdownEditor.Fork/js/marked.min.js";
+        internal static readonly string CodeMirrorThemesUrl = "_content/PSC.Blazor.Components.MarkdownEditor.Fork/css/codemirror/";
     }
 }
